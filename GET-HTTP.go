89495be6bfb5168d817e84e78843c6759314be98
andreichenko256 <andreichenko.kirill2000@gmail.com>
2020-09-27 15:47:35 +0300

    SecondMerge commit

--- GET-HTTP.go
+++ GET-HTTP.go
@@ -1,1 +1,22 @@
-1
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, errGet := http.Get("http://localhost:8795/time")
+	if errGet != nil {
+		fmt.Println(errGet)
+		return
+	}
+	defer resp.Body.Close()
+	body, errResp := ioutil.ReadAll(resp.Body)
+	if errResp != nil {
+		fmt.Println(errResp)
+	}
+	fmt.Println(string(body))
+}
+
