6be3b7bbd18722c0c2a92fc39c67bbd439fd2c25
dimakryshtal <dima.krshl@gmail.com>
2020-09-27 15:44:03 +0300

    firstMerge

--- server.go
+++ server.go
@@ -1,1 +1,33 @@
-1
+package main
+
+import (
+    "fmt"
+    "log"
+	"net/http"
+	"time"
+	"encoding/json"
+)
+
+type Time1 struct  {
+	Time string `json:"time"`
+}
+
+func main() {
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request){
+		t := time.Now().Format(time.RFC3339);
+		
+		json1, err := json.MarshalIndent(Time1{
+			Time: t,
+		},  "", " ")
+		if err != nil {
+			fmt.Fprintf(w, err.Error());
+			return
+		}
+        fmt.Fprintf(w, string(json1))
+    })
+
+	fmt.Printf("Starting server at port 8795\n")
+    if err := http.ListenAndServe(":8795", nil); err != nil {
+        log.Fatal(err)
+    }
+}
